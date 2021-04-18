2a4096fefb13fcc8326a529bda79986c6c6b62d6
jeon0050-al <77302961+jeon0050-al@users.noreply.github.com>
2021-04-18 12:57:43 -0400

    summary

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -1,29 +1,32 @@
 window.onload = init;
 
-function showHideMobileMenu() {
+function init() {
 
-	var mobileNav = document.querySelector('.mobile-nav');
+	$('#form').submit(function (e) {
+   		 e.preventDefault();
+   		 $("#results").removeClass('reveal');
+   		 var form = this;
+   		 $(".overlay-container").fadeIn(1000, function(){
+   		 		showformValues(form);
+   		 		$('.overlay-container').delay(500).fadeOut(500);
+   		 		$("#results").addClass('reveal');
+   		 });
+	});
 
-	// in a if statement == is used to compare two value, if the values matches then the condition is true
-	// an if statement is followed by an else statement which runs when the given condition is not met
-	if(mobileNav.style.display=="block") {
-		mobileNav.style.display="none";
-	} else {
-		mobileNav.style.display="block";
-	}
-}
-function init() {	
-	// document.querySelector('.submit').onclick = submit;
-	
-	document.querySelector('.ham').onclick = showHideMobileMenu;
-	// $('#form').submit(function (e) {
-//    		 e.preventDefault();
-// });
-}
-function HideNav() {
-	var navbar = document.getElementById('nav')
 }
 
+// show the form values in the results div 
+function showformValues(form){
+	var formValues = $(form).serializeArray(); 	
+	$.each(formValues, function(index, field){
+		$("#results").find("#"+field.name+"_result").text(field.value);
 
-   		 
-    
+		// special check for email to add a link instead of just string
+		if(field.name=="email"){
+			$("#results").find("#"+field.name+"_result").attr("href", "mailto:"+field.value);
+		}
+	});				
+}
+
+
+
